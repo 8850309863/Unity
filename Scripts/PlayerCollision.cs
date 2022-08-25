c57bb45aec470229a13c1357e12d1aa4aec316ce
Nishant Walunj <70265091+8850309863@users.noreply.github.com>
2022-08-25 12:23:17 +0530

    Add files via upload

---
+++ Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{       
+    public Playermovement movement;
+       
+    
+
+    void OnCollisionEnter(Collision Collisioninfo)
+    {
+
+            if(Collisioninfo.collider.tag=="Obstacle")
+            {
+
+                    movement.enabled = false;
+                    FindObjectOfType<GameManager>().EndGame();
+            }
+    }
+        
+  
+   
+}
---
+++ Scripts/Playermovement.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{  public Rigidbody rb;
+   
+      
+     public float forwardForce=1000f;
+     public float sidewaysforce=1f;
+     public VariableJoystick joystick;
+  
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {       
+        
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+        
+        
+        Vector3 direction =Vector3.forward*joystick.Vertical + Vector3.right * joystick.Horizontal;
+        rb.AddForce(direction *sidewaysforce*Time.deltaTime,ForceMode.VelocityChange);
+    
+        
+    
+          if(rb.position.y < -1f)
+    {
+        FindObjectOfType<GameManager>().EndGame();
+    }
+    }
+}
+    
